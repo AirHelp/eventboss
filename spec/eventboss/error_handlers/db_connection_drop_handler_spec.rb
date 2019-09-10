2169a0f5e7eae2561d2d7694c6a9faad63b2ec33
Paweł Przeniczny <pawel.przeniczny@airhelp.com>
2019-09-10 15:26:48 +0200

    Drop rails dev dependency.

--- spec/eventboss/error_handlers/db_connection_drop_handler_spec.rb
+++ spec/eventboss/error_handlers/db_connection_drop_handler_spec.rb
@@ -1,16 +1,23 @@
-require 'active_record'
-require 'eventboss/error_handlers/db_connection_drop_handler'
+require 'spec_helper'
 
 describe Eventboss::ErrorHandlers::DbConnectionDropHandler do
+  module ActiveRecord
+    class StatementInvalid
+    end
+
+    class Base
+      def self.clear_active_connections!; end
+    end
+  end
+
   subject { described_class.new }
 
-  let(:statement_invalid_error) { double(class: ActiveRecord::StatementInvalid) }
   let(:some_error) { double(class: StandardError) }
 
   context 'when receives ActiveRecord::StatementInvalid exception' do
     it 'calls AR.clear_active_connections!' do
-      expect(::ActiveRecord::Base).to receive(:clear_active_connections!)
-      subject.call(statement_invalid_error)
+      expect(ActiveRecord::Base).to receive(:clear_active_connections!)
+      subject.call(ActiveRecord::StatementInvalid.new)
     end
   end
 
