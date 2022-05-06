61001aa62571ca2d3c87ee9407fa009e6698319e
Paweł Przeniczny <pawel.przeniczny@airhelp.com>
2022-05-06 13:02:22 +0200

    Rewrite Topic specs to test actual behaviour of methods

--- spec/eventboss/topic_spec.rb
+++ spec/eventboss/topic_spec.rb
@@ -4,77 +4,46 @@ require "spec_helper"
 
 describe Eventboss::Topic do
   describe '.build_arn' do
-    subject { described_class.build_arn(event_name: 'J', source_app: 'CH') }
+    subject { described_class.build_arn(event_name: 'summer_party', source_app: 'jigger') }
 
     before do
-      allow(Eventboss.configuration).to receive(:eventboss_region)
-      allow(Eventboss.configuration).to receive(:eventboss_account_id)
-      allow(Eventboss::Topic).to receive(:build_name)
+      allow(Eventboss.configuration).to receive(:eventboss_region).and_return('eu-west-1')
+      allow(Eventboss.configuration).to receive(:eventboss_account_id).and_return(123098)
+      allow(Eventboss.configuration).to receive(:sns_sqs_name_infix).and_return('eventbus')
+      allow(Eventboss).to receive(:env).and_return('development')
     end
 
-    it 'includes aws arn part' do
-      expect(subject).to include('arn:aws:sns')
+    it "contains the region, account id, infix, application name, event name and environment" do
+      expect(subject).to eql('arn:aws:sns:eu-west-1:123098:eventbus-jigger-summer_party-development')
     end
 
-    it 'uses eventboss region' do
-      subject
-      expect(Eventboss.configuration).to have_received(:eventboss_region)
-    end
+    context 'when source application not passed' do
+      subject { described_class.build_arn(event_name: 'summer_party') }
 
-    it 'uses eventboss account id' do
-      subject
-      expect(Eventboss.configuration).to have_received(:eventboss_account_id)
-    end
-
-    it 'passes params to .build_name' do
-      subject
-      expect(Eventboss::Topic).to have_received(:build_name)
-        .with(event_name: 'J', source_app: 'CH')
-    end
-
-    context 'when source app not given' do
-      subject { described_class.build_arn(event_name: 'J') }
-
-      it 'is passed as nil' do
-        subject
-        expect(Eventboss::Topic).to have_received(:build_name)
-          .with(event_name: 'J', source_app: nil)
+      it "doesn't contain application name" do
+        expect(subject).to eql('arn:aws:sns:eu-west-1:123098:eventbus-summer_party-development')
       end
+
     end
   end
 
   describe '.build_name' do
-    subject { described_class.build_name(event_name: 'J', source_app: 'CH') }
+    subject { described_class.build_name(event_name: 'summer_party', source_app: 'jigger') }
 
     before do
-      allow(Eventboss).to receive(:env)
-      allow(Eventboss.configuration).to receive(:sns_sqs_name_infix)
+      allow(Eventboss.configuration).to receive(:sns_sqs_name_infix).and_return('eventbus')
+      allow(Eventboss).to receive(:env).and_return('development')
     end
 
-    it 'includes required params' do
-      expect(subject).to include('CH-J')
+    it "contains the infix, application name, event name and environment" do
+      expect(subject).to eql('eventbus-jigger-summer_party-development')
     end
 
-    it 'uses eventboss env' do
-      subject
-      expect(Eventboss).to have_received(:env)
-    end
+    context 'when source application not passed' do
+      subject { described_class.build_name(event_name: 'summer_party') }
 
-    it 'uses eventboss sns_sqs_name_infix' do
-      subject
-      expect(Eventboss.configuration).to have_received(:sns_sqs_name_infix)
-    end
-
-    context 'when source app not passed' do
-      subject { described_class.build_name(event_name: 'J') }
-
-      before do
-        allow(Eventboss.configuration).to receive(:sns_sqs_name_infix)
-          .and_return('infix')
-      end
-
-      it 'event name is right after the infix' do
-        expect(subject).to include('infix-J')
+      it "doesn't contain application name" do
+        expect(subject).to eql('eventbus-summer_party-development')
       end
     end
   end
