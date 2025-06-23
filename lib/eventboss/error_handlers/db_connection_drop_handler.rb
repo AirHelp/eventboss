813131c14050983a034026ff078858c32f877192
Andrzej Bisewski <152981096+andrzejbisewski@users.noreply.github.com>
2025-06-23 11:45:49 +0200

    Update db_connection_drop_handler.rb

--- lib/eventboss/error_handlers/db_connection_drop_handler.rb
+++ lib/eventboss/error_handlers/db_connection_drop_handler.rb
@@ -6,7 +6,7 @@ module Eventboss
       def call(exception, _context = {})
         return unless exception.is_a?(::ActiveRecord::StatementInvalid)
 
-        if Gem::Version.new(ActiveRecord::VERSION::STRING) >= Gem::Version.new('8.0.0')
+        if Gem::Version.new(ActiveRecord::VERSION::STRING) < Gem::Version.new('8.0.0')
           ::ActiveRecord::Base.clear_active_connections!
         else
           ::ActiveRecord::Base.connection_handler.clear_active_connections!
