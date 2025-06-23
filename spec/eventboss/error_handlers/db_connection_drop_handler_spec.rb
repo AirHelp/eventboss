19c3035d85f73542b1f090daf044717626e26950
Andrzej Bisewski <152981096+andrzejbisewski@users.noreply.github.com>
2025-06-23 10:43:43 +0200

    Fix clear_active_connections for newer rails

--- spec/eventboss/error_handlers/db_connection_drop_handler_spec.rb
+++ spec/eventboss/error_handlers/db_connection_drop_handler_spec.rb
@@ -1,30 +1,66 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 describe Eventboss::ErrorHandlers::DbConnectionDropHandler do
   module ActiveRecord
-    class StatementInvalid
+    module VERSION
+      STRING = '8.0.0'
+    end
+
+    class StatementInvalid < StandardError; end
+
+    class ConnectionHandler
+      def clear_active_connections!; end
     end
 
     class Base
-      def self.clear_active_connections!; end
+      class << self
+        def clear_active_connections!; end
+        def connection_handler; end
+      end
     end
   end
 
+  let(:handler) { instance_double(ActiveRecord::ConnectionHandler) }
+
   subject { described_class.new }
 
-  let(:some_error) { double(class: StandardError) }
+  before do
+    allow(ActiveRecord::Base).to receive(:connection_handler).and_return(handler)
+  end
 
-  context 'when receives ActiveRecord::StatementInvalid exception' do
-    it 'calls AR.clear_active_connections!' do
+  context 'when ActiveRecord version >= 8.0.0' do
+    before do
+      stub_const('ActiveRecord::VERSION::STRING', '8.0.0')
+    end
+
+    it 'calls Base.clear_active_connections!' do
       expect(ActiveRecord::Base).to receive(:clear_active_connections!)
       subject.call(ActiveRecord::StatementInvalid.new)
     end
   end
 
-  context 'when receives some other exception' do
-    it 'doesnt call AR.clear_active_connections!' do
-      expect(::ActiveRecord::Base).to_not receive(:clear_active_connections!)
-      subject.call(some_error)
+  context 'when ActiveRecord version < 8.0.0' do
+    before do
+      stub_const('ActiveRecord::VERSION::STRING', '7.2.3')
+    end
+
+    it 'calls connection_handler.clear_active_connections!' do
+      expect(handler).to receive(:clear_active_connections!)
+      subject.call(ActiveRecord::StatementInvalid.new)
+    end
+  end
+
+  context 'when receives a different exception' do
+    before do
+      stub_const('ActiveRecord::VERSION::STRING', '8.0.0')
+    end
+
+    it 'does not clear any connections' do
+      expect(ActiveRecord::Base).not_to receive(:clear_active_connections!)
+      expect(handler).not_to receive(:clear_active_connections!)
+      subject.call(StandardError.new)
     end
   end
 end
