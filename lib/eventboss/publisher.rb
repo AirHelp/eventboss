9e2565271b620c16d05d10f8b243783231bbf289
Andrzej Bisewski <152981096+andrzejbisewski@users.noreply.github.com>
2025-08-20 09:33:15 +0200

    Update publisher.rb

--- lib/eventboss/publisher.rb
+++ lib/eventboss/publisher.rb
@@ -9,57 +9,68 @@ module Eventboss
       @source = configuration.eventboss_app_name unless opts[:generic]
     end
 
+    # Publishes the payload to the SNS topic.
+    # If Sentry is enabled, it wraps the publish action in a Sentry span for tracing.
     def publish(payload)
-      if defined?(::Eventboss::Sentry::Integration) && ::Sentry.initialized?
-        publish_with_sentry(payload)
+      sns_params = build_sns_params(payload)
+      publisher_action = -> { sns_client.publish(sns_params) }
+
+      if sentry_enabled?
+        with_sentry_span(&publisher_action)
       else
-        publish_without_sentry(payload)
+        publisher_action.call
       end
-    end
-
-    private
-
-    def publish_with_sentry(payload)
-      topic_arn = Topic.build_arn(event_name: event_name, source_app: source)
-
-      ::Sentry.with_child_span(op: "queue.publish", description: "Eventboss push #{source}/#{event_name}") do
-        sns_client.publish(
-          topic_arn: topic_arn,
-          message: json_payload(payload),
-          message_attributes: build_sentry_message_attributes
-        )
-      end
-    end
-
-    def publish_without_sentry(payload)
-      topic_arn = Topic.build_arn(event_name: event_name, source_app: source)
-      sns_client.publish(
-        topic_arn: topic_arn,
-        message: json_payload(payload)
-      )
     end
 
     private
 
     attr_reader :event_name, :sns_client, :configuration, :source
 
+    def sentry_enabled?
+      defined?(::Eventboss::Sentry::Integration) && ::Sentry.initialized?
+    end
+
+    def build_sns_params(payload)
+      {
+        topic_arn: Topic.build_arn(event_name: event_name, source_app: source),
+        message: json_payload(payload),
+        message_attributes: sentry_enabled? ? build_sentry_message_attributes : {}
+      }
+    end
+
+    def with_sentry_span
+      queue_name = Queue.build_name(destination: source, event_name: event_name, env: Eventboss.env, source_app: source)
+
+      ::Sentry.with_child_span(op: 'queue.publish', description: "Eventboss push #{source}/#{event_name}") do |span|
+        span.set_data(::Sentry::Span::DataConventions::MESSAGING_DESTINATION_NAME, Eventboss::Sentry::ServerMiddleware::QUEUES_WITHOUT_ENV[queue_name])
+
+        message = yield # Executes the publisher_action lambda
+
+        span.set_data(::Sentry::Span::DataConventions::MESSAGING_MESSAGE_ID, message.message_id)
+        message
+      end
+    end
+
     def json_payload(payload)
       payload.is_a?(String) ? payload : payload.to_json
     end
 
+    # Constructs SNS message attributes for Sentry trace propagation.
     def build_sentry_message_attributes
       attributes = ::Sentry.get_trace_propagation_headers
                            .slice('sentry-trace', 'baggage')
                            .transform_values do |header_value|
         { string_value: header_value, data_type: 'String' }
       end
-      user = ::Sentry.get_current_scope.user
+
+      user = ::Sentry.get_current_scope&.user
       if user && !user.empty?
         attributes['sentry_user'] = {
           string_value: user.to_json,
           data_type: 'String'
         }
       end
+
       attributes
     end
   end
