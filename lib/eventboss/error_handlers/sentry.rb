803af4dadb248dab7552d5c0f225cf1382e43241
Andrzej Bisewski <152981096+andrzejbisewski@users.noreply.github.com>
2025-08-20 08:45:56 +0200

    documentation

--- lib/eventboss/error_handlers/sentry.rb
+++ lib/eventboss/error_handlers/sentry.rb
@@ -1,6 +1,12 @@
 module Eventboss
   module ErrorHandlers
     class Sentry
+      def initialize
+        warn "[DEPRECATED] Eventboss::ErrorHandlers::Sentry is deprecated. " \
+             "Use Eventboss::Sentry::ErrorHandler instead. " \
+             "For automatic configuration, require 'eventboss/sentry/configure'. " \
+             "This class will be removed in a future version."
+        super
       def call(exception, context = {})
         eventboss_context = { component: 'eventboss' }
         eventboss_context[:action] = context[:processor].class.to_s if context[:processor]
